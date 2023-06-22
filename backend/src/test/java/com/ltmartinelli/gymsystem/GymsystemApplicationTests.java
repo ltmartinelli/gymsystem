33242ff901c0696788acb70eac8a18e962309c1b
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-22 16:45:45 -0300

    Project Start

---
+++ backend/src/test/java/com/ltmartinelli/gymsystem/GymsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltmartinelli.gymsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
