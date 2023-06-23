9c0e9f2bfaa7c9f40e5fb6c93a1ca77cd0abe896
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-23 18:03:16 -0300

    Constructors, Getters and Settes for Unit and Address, Unit Address

--- backend/src/main/java/com/ltmartinelli/gymsystem/entities/Unit.java
+++ backend/src/main/java/com/ltmartinelli/gymsystem/entities/Unit.java
@@ -2,6 +2,8 @@ package com.ltmartinelli.gymsystem.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_unit")
 public class Unit {
@@ -15,4 +17,57 @@ public class Unit {
     @OneToOne(mappedBy = "unit", cascade = CascadeType.ALL)
     private UnitAddress address;
 
+    public Unit() {
+    }
+
+    public Unit(Long id, String name, UnitAddress address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UnitAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(UnitAddress address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Unit unit)) return false;
+        return Objects.equals(id, unit.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Unit{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
 }
--- backend/src/main/java/com/ltmartinelli/gymsystem/entities/UnitAddress.java
+++ backend/src/main/java/com/ltmartinelli/gymsystem/entities/UnitAddress.java
@@ -6,10 +6,34 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_unit_adress")
+@Table(name = "tb_unit_address")
 public class UnitAddress extends Address {
 
     @OneToOne
     @MapsId
     private Unit unit;
+
+    public UnitAddress() {
+    }
+
+    public UnitAddress(Long id, String state, String city, Integer number, String zip, Unit unit) {
+        super(id, state, city, number, zip);
+        this.unit = unit;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "UnitAddress{" +
+                "unit=" + unit +
+                super.toString() +
+                "} ";
+    }
 }
