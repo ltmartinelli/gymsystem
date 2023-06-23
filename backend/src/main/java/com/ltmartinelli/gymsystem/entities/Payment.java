5c02e38dbf754e7a24510587a15d27b4cac153e0
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-23 19:03:13 -0300

    Payment

---
+++ backend/src/main/java/com/ltmartinelli/gymsystem/entities/Payment.java
@@ -0,0 +1,99 @@
+package com.ltmartinelli.gymsystem.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant paymentDate;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant dueDate;
+
+    private PaymentStatus status;
+
+    private Double price;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant paymentDate, Instant dueDate, PaymentStatus status, Double price) {
+        this.id = id;
+        this.paymentDate = paymentDate;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Instant paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Instant getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Instant dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Payment payment)) return false;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "id=" + id +
+                ", paymentDate=" + paymentDate +
+                ", dueDate=" + dueDate +
+                ", status=" + status +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ backend/src/main/java/com/ltmartinelli/gymsystem/entities/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.ltmartinelli.gymsystem.entities;
+
+public enum PaymentStatus {
+    PENDING,
+    PAID,
+    CANCELLED,
+    EXPIRED,
+}
