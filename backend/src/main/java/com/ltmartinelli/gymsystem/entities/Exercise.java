ca7f89cd3eb5f3514cd19b82d68e99bafe69946d
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-23 01:33:12 -0300

    Exercise

---
+++ backend/src/main/java/com/ltmartinelli/gymsystem/entities/Exercise.java
@@ -0,0 +1,93 @@
+package com.ltmartinelli.gymsystem.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_exercise")
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer sets;
+    private Integer reps;
+    private Integer weight;
+
+    public Exercise() {
+    }
+
+    public Exercise(Long id, String name, Integer sets, Integer reps, Integer weight) {
+        this.id = id;
+        this.name = name;
+        this.sets = sets;
+        this.reps = reps;
+        this.weight = weight;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSets() {
+        return sets;
+    }
+
+    public void setSets(Integer sets) {
+        this.sets = sets;
+    }
+
+    public Integer getReps() {
+        return reps;
+    }
+
+    public void setReps(Integer reps) {
+        this.reps = reps;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Exercise exercise)) return false;
+        return Objects.equals(id, exercise.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Exercise{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sets=" + sets +
+                ", reps=" + reps +
+                ", weight=" + weight +
+                '}';
+    }
+}
