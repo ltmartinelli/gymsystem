4266739c02da34814b62381da6b082684a49bab3
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-23 21:29:38 -0300

    Seed Start

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_unit (name) VALUES ('Power Canoas')
+
+INSERT INTO tb_user (email, name, password, phone, birth_date, unit_id) VALUES ('joao@gmail.com', 'João', 'wqd6sa876d', '(51)99999-9999', '2003-02-14', 1)
+
+
+
+
+
+
