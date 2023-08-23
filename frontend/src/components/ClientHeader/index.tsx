cb51152d7abdc15363ff899834ee7205760fd130
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-08-23 15:34:57 -0300

    LoggedUser Header Components

--- frontend/src/components/ClientHeader/index.tsx
+++ frontend/src/components/ClientHeader/index.tsx
@@ -1,11 +1,20 @@
+import { useContext } from 'react';
 import './styles.css'
+import { ContextToken } from '../../utils/context-token';
+import { Link } from 'react-router-dom';
+import LoggedUser from '../LoggedUser';
 
 export default function ClientHeader()
 {
+
+    const { contextTokenPayload } = useContext(ContextToken);
+
     return (
         <header className='gs-header-client'>
-            <h1 className='gs-header-title'>GymSystem</h1>
-            <h2>Login</h2>
+            <Link to='/'>
+                <h1 className='gs-header-title'>GymSystem</h1>
+            </Link>
+            <LoggedUser/>
         </header>
     )
 }
---
+++ frontend/src/components/LoggedUser/index.tsx
@@ -0,0 +1,33 @@
+import { Link, useNavigate } from 'react-router-dom';
+import * as authService from '../../services/auth-service.ts'
+import { useContext } from 'react';
+import { ContextToken } from '../../utils/context-token.ts';
+
+export default function LoggedUser()
+{
+    const { contextTokenPayload, setContextTokenPayload } = useContext(ContextToken);
+    const navigate = useNavigate();
+
+    function handleLogoutClick()
+    {
+        authService.logout();
+        setContextTokenPayload(undefined);
+        navigate("/login");
+    }
+
+    return (
+        contextTokenPayload && authService.isAuthenticated() ?
+            (
+                <div className="gs-logged-user">
+                    <p>{contextTokenPayload.user_name}</p>
+                    <span onClick={handleLogoutClick}>Sair</span>
+                </div>
+            )
+            :
+            (
+                <Link to="/login">
+                    Login
+                </Link>
+            )
+    );
+}
