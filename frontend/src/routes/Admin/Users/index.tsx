13dadc111ff15936728a09e8365dfd9f3871c43b
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-09-19 17:34:02 -0300

    Users Layout

--- frontend/src/routes/Admin/Users/index.tsx
+++ frontend/src/routes/Admin/Users/index.tsx
@@ -1,6 +1,38 @@
+import './styles.css'
+
 export default function Users()
 {
     return (
-        <h1>teste</h1>
+        <section className="gs-users-section">
+            <table className="gs-users-table">
+                <thead>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Unidade</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Fulano</td>
+                        <td>fulano@gmail.com</td>
+                        <td>unidade</td>
+                    </tr>
+                    <tr>
+                        <td>Fulano</td>
+                        <td>fulano@gmail.com</td>
+                        <td>unidade</td>
+                    </tr>
+                    <tr>
+                        <td>Fulano</td>
+                        <td>fulano@gmail.com</td>
+                        <td>unidade</td>
+                    </tr>
+                    <tr>
+                        <td>Fulano</td>
+                        <td>fulano@gmail.com</td>
+                        <td>unidade</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
     );
 }
